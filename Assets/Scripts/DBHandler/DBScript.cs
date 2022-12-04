3774ac9b2f667b678c8aebe7d674544afd0c3e62
emil zarrinfar <emzar21@student.sdu.dk>
2022-12-05 00:52:24 +0100

    Big leaderboard setup

---
+++ Assets/Scripts/DBHandler/DBScript.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+
+public class DBScript : MonoBehaviour
+{
+    //Stores name of DataBase
+    private string dbName = "URI=file:myDB.db";
+    
+    void Start()
+    {
+
+        
+        
+        CreateDB();
+        AddScore("stine",69);
+        ReadLB();
+
+    }
+
+
+
+    public void CreateDB()
+    {
+        //creates connection to DataBase
+        using(var connection = new SqliteConnection(dbName))
+        {
+            //opens connection (DUH)
+            connection.Open();
+
+
+            using(var command = connection.CreateCommand())
+            {
+                //creates the table scoreboard if not already existing
+                command.CommandText = "CREATE TABLE IF NOT EXISTS scoreboard(id INTEGER PRIMARY KEY,name VARCHAR(3), score INT)";
+                
+
+                //Execute Order 66
+                command.ExecuteNonQuery();
+
+            }
+
+            //i wonder what this does  :             ^)
+            connection.Close();
+
+
+        }
+
+
+    }
+
+    public void AddScore(string name, int score){
+         
+         
+          using(var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+
+
+            using(var command = connection.CreateCommand())
+            {
+                
+                command.CommandText = $"INSERT INTO scoreboard(name,score) VALUES('{name}',{score})";
+                
+
+                //Execute Order 66
+                command.ExecuteNonQuery();
+
+            }
+
+            connection.Close();
+        }
+
+    }
+
+
+
+
+    public void ReadLB()
+    {
+        using(var connection = new SqliteConnection(dbName))
+        {
+            
+            connection.Open();
+            
+            
+
+
+            using(var command = connection.CreateCommand())
+            {
+                
+                //takes all the info from table
+                command.CommandText = "SELECT * FROM scoreboard ORDER BY score DESC";
+
+
+                //for every iterations in table
+                using(IDataReader reader = command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        Debug.Log("Name: "+ reader["name"] + "Score: "+ reader["score"]);
+
+                    }
+
+                    reader.Close();
+                }
+            }
+
+            //i wonder what this does  :             ^)
+            connection.Close();
+
+
+        }
+
+
+    }
+
+
+ 
+}
